0fd8fa0e23c841ad29564a95fd0dd63d891e283f
ChayaGreisman <chayamgreisman@gmail.com>
2020-10-27 10:35:04 -0400

    o(n^2) example

--- JS Algo + Data Structures Masterclass Notes/frequency.js
+++ JS Algo + Data Structures Masterclass Notes/frequency.js
@@ -6,7 +6,25 @@
 // same([1,2,3], [1,9])      // false
 // same([1,2,1], [4,4,1])    // false (must be same frequency)
 
+// O(n^2)
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    for(let i = 0; i < arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1) {
+            return false;
+        }
+        console.log(arr2);
+        arr2.splice(correctIndex,1)
+    }
+    return true;
+}
 
+same([1,2,3,2], [9,1,4,4])
+
+// O(n)
 function same(arr1, arr2){
     if(arr1.length !== arr2.length){
         return false;
