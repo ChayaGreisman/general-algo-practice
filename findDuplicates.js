026cae758007f9b50eb6eec710d032cdf3295d4b
ChayaGreisman <chayamgreisman@gmail.com>
2020-11-05 19:14:40 -0500

    find duplicates- two pointer solution

--- findDuplicates.js
+++ findDuplicates.js
@@ -1,3 +1,23 @@
 
 // find duplicates in 2 sorted arrays 
 
+
+// two pointer method
+// O(m+n)
+function findDuplicates(arr1,arr2){
+    let arr = []
+    let pointer1 = 0
+    let pointer2 = 0
+    while (pointer1<arr1.length && pointer2<arr2.length){ 
+      if(arr1[pointer1]===arr2[pointer2]){
+        arr.push(arr1[pointer1])
+        pointer1++
+        pointer2++
+      } else if (arr1[pointer1] < arr2[pointer2]){
+        pointer1++
+      } else {
+        pointer2++
+      }
+    }
+    return arr
+}
